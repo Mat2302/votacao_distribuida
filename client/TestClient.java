b478cd4ff0173ee7017cc994518cdd7815639f07
Hitallo <azevedohitallo@gmail.com>
2025-11-18 16:09:02 -0300

    alterando nome das pastas para ingles

--- client/TestClient.java
+++ client/TestClient.java
@@ -1,4 +1,4 @@
-package cliente;
+package client;
 
 import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
--- server/ClientHandler.java
+++ server/ClientHandler.java
@@ -1,4 +1,4 @@
-package servidor;
+package server;
 import java.io.IOException;
 import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
